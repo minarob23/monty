00da2b7a08c5154fe639a9085720c73e547e8a4d
Mina Robir <menarobir246rm@gmail.com>
2023-08-24 23:45:29 +0300

    Create add.c

---
+++ add.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+global_t gl;
+/**
+ * Add - add two elements of stacks
+ * @stack: stack head
+ * @line_number: line num
+ * Return: void
+ */
+
+void Add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *aux = *stack;
+
+	if (aux && aux->next)
+	{
+		aux = aux->next;
+		aux->n = aux->n + (*stack)->n;
+		_pop(stack, line_number);
+	}
+	else
+	{
+		free(gl.buffer), free_list(*stack), fclose(gl.fd);
+		dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
