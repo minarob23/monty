774cf3a66d7cf8dfd0368098528b95f6ad92852a
Mina Robir <menarobir246rm@gmail.com>
2023-08-25 00:10:49 +0300

    Create nop.c

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+global_t gl;
+/**
+ * Nop - The opcode nop does not do anything.
+ * @stack: stack head
+ * @line_number: line num
+ * Return: void
+ */
+
+void Nop(stack_t **stack, unsigned int line_number)
+{
+	line_number = line_number;
+	stack = stack;
+}
