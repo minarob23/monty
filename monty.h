11859610f6c2da60b4dc6e4f68e6506d3b740487
Mina Robir <menarobir246rm@gmail.com>
2023-08-24 23:52:59 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -70,7 +70,7 @@ void _swap(stack_t **stack, unsigned int line_number);
 void Add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
 void _sub(stack_t **stack, unsigned int line_number);
-void _div(stack_t **stack, unsigned int line_number);
+void Divide(stack_t **stack, unsigned int line_number);
 void _mul(stack_t **stack, unsigned int line_number);
 void _mod(stack_t **stack, unsigned int line_number);
 void _pchar(stack_t **stack, unsigned int line_number);
