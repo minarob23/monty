3faa9b788e61a11a6414991141f7ffd91940eee4
Mina Robir <menarobir246rm@gmail.com>
2023-08-24 19:25:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ bus_t bus = {NULL, NULL, NULL, 0};
 * main - monty code interpreter
 * @argc: number of arguments
 * @argv: monty file location
-* Return: 0 on success
+* Return: 0 success
 */
 int main(int argc, char *argv[])
 {
