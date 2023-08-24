f9ebc9988bce7358a0dabce4be455783d54de378
Mina Robir <menarobir246rm@gmail.com>
2023-08-25 00:36:54 +0300

    Create pull.c

---
+++ pull.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * _pall - pull the stack all 
+ * @stack: head
+ * @line_number: line_number
+ * Return: void
+ */
+void _pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h;
+
+	line_number = line_number;
+	if (stack && *stack)
+	{
+		h = *stack;
+		while (h->next)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+		if (h)
+			printf("%d\n", h->n);
+	}
+}
