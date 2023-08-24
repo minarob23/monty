cb0f7786a7c1c161d32fd26a2e2a2fc758819987
Mina Robir <menarobir246rm@gmail.com>
2023-08-24 23:47:02 +0300

    Create add_node.c

---
+++ add_node.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * add_node_end - add a new node at the end
+ * @head: head
+ * @n: value new node
+ * Return: new node adress
+ */
+stack_t *add_node_end(stack_t **head, int n)
+{
+	stack_t *new = NULL, *aux = *head;
+
+	new = malloc(sizeof(stack_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head)
+	{
+		while (aux->next)
+		{
+			aux = aux->next;
+		}
+		aux->next = new;
+		new->prev = aux;
+	}
+	else
+	{
+		new->prev = NULL;
+		*head = new;
+	}
+	return (new);
+}
